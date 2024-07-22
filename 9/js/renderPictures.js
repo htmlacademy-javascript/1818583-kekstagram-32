bca158e36536b7e604b386a492a8615e761d1e39
Keks <keks@htmlacademy.ru>
2024-07-22 21:02:56 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/renderPictures.js
@@ -0,0 +1,37 @@
+import {renderBigPicture} from './renderBigPicture';
+
+const renderPictures = (photos) => {
+  const template = document.querySelector('#picture').content;
+  const templateContent = template.querySelector('.picture');
+  const container = document.querySelector('.pictures');
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < photos.length; i++) {
+    const {comments, description, likes, url, id} = photos[i];
+    const clone = templateContent.cloneNode(true);
+
+    const img = clone.querySelector('.picture__img');
+    img.src = url;
+    img.alt = description;
+    const likesElement = clone.querySelector('.picture__likes');
+    likesElement.textContent = likes;
+    const commentsElement = clone.querySelector('.picture__comments');
+    commentsElement.textContent = comments.length;
+    clone.dataset.photoId = id;
+
+    fragment.appendChild(clone);
+  }
+
+  container.appendChild(fragment);
+
+  container.addEventListener('click', (e) => {
+    const link = e.target.closest('a');
+
+    if (link) {
+      const photoData = photos.find((photo) => String(photo.id) === link.dataset.photoId);
+      renderBigPicture(photoData);
+    }
+  });
+};
+
+export {renderPictures};
